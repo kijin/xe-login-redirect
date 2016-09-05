6111832dd5f2687855e033bacf693d6fc0e4cf01
Kijin Sung <kijin@kijinsung.com>
2016-09-05 18:22:10 +0900

    Initial commit

---
+++ login_redirect.addon.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file login_redirect.addon.php
+ * @author Kijin Sung <kijin@kijinsung.com>
+ * @license GPLv2 or Later <https://www.gnu.org/licenses/gpl-2.0.html>
+ */
+if (!defined('__XE__')) exit();
+
+/**
+ * Detect duplicates before module action.
+ */
+if ($called_position === 'before_module_init' && $addon_info->redirect_url)
+{
+	$logged_info = Context::get('logged_info');
+	if (!$logged_info || !$logged_info->member_srl)
+	{
+		$addon_info->redirect_url = trim($addon_info->redirect_url);
+		if (preg_match('/^(?:disp|proc)Member/', Context::get('act')))
+		{
+			return;
+		}
+		if ($_SERVER['REQUEST_URI'] === $addon_info->redirect_url)
+		{
+			return;
+		}
+		if (preg_match('/^[a-z0-9_]+$/i', $addon_info->redirect_url) && Context::get('mid') === $addon_info->redirect_url)
+		{
+			return;
+		}
+		if (getNotEncodedFullUrl() === $addon_info->redirect_url)
+		{
+			return;
+		}
+		
+		if (!headers_sent())
+		{
+			header('Location: ' . $addon_info->redirect_url);
+			exit;
+		}
+	}
+}
